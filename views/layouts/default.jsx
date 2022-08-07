80e900129a0eb7f8f19417119a2212104cac04cd
Menegoth <antonioareas@me.com>
2022-08-06 19:10:30 -0700

    sending index and show data

---
+++ views/layouts/default.jsx
@@ -0,0 +1,21 @@
+//dependencies
+const React = require("react");
+
+function Default(html) {
+    return (
+        <html>
+            <head>
+                <title>Default</title>
+            </head>
+            <body>
+                <h1>HTML Rendered!</h1>
+                <div className="container">
+                {html.children}                    
+                </div>
+            </body>
+        </html>
+    )
+}
+
+//export
+module.exports = Default;
